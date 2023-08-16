5bb0a90f6b5446ab349fc1d1535cd071670e0266
IanPC <iancaminhas25@hotmail.com>
2023-08-16 16:03:53 -0300

    Criando rota de admin

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,19 @@
+import { GetServerSideProps } from 'next'
 import styles from './styles.module.css'
 import Head from 'next/head'
+import { getSession } from "next-auth/react";
 
 export default function Dashboard() {
-/*
-    Head é para alterar o title da aba
-*/
+
+    /*Essa é uma solução que poderia ser usada: quando o user carregar o componente na tela, fazemos um 
+    useEffect(()), chamar o hook do useSession e verificar se
+    tem usuário logado...
+    A solução econtrada foi com getServerSideProps 
+    */
+
+    /*
+        Head é para alterar o title da aba
+    */
     return (
         <div className={styles.container}>
             <Head>
@@ -14,4 +23,28 @@ export default function Dashboard() {
             <h1>Página painel</h1>
         </div>
     )
+}
+/*Esse getServerSideProps é executado no lado servidor. Vai mostrar 
+no terminal esse console.log("BUSCANDO PELO SERVER SIDE")).
+Antes de montar a interface, já foi passado no servidor
+Conseguimos usar algo melhor que o useEffect().
+Aqui olho se tem usuário logado ou não
+*/
+export const getServerSideProps: GetServerSideProps = async ({req}) => {
+    //console.log("BUSCANDO PELO SERVER SIDE")
+    //Estou esperando a ter a sessão. Vai ter ou não ? Essa session vai ser retornada da proppriedade getServerSession
+    const session = await getSession({req})
+    //console.log(session)
+    if(!session?.user) {
+        //Se não tem usuário, vamos redirecionar para a home(no caso é o /)
+        return{
+            redirect: {
+                destination: '/',
+                permanent: false,
+            }
+        }
+    }
+    return {
+        props:{},
+    }
 }
